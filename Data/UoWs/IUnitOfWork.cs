a5ffb32d7e54007dbacbe9f00d457894425653de
Y-Baker <yuossefahmedgaber@gmail.com>
2025-01-22 16:10:21 +0200

    Unit Of Work

---
+++ Data/UoWs/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace EG_ERP.Data.UoWs;
+
+public interface IUnitOfWork
+{
+
+}
---
+++ Data/UoWs/UnitOfWork.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Threading.Tasks;
+using EG_ERP.Data.Repos;
+using EG_ERP.Models;
+
+namespace EG_ERP.Data.UoWs;
+
+public class UnitOfWork : IUnitOfWork, IAsyncDisposable
+{
+    private readonly AppDbContext db;
+    private bool disposed = false;
+    public UnitOfWork(AppDbContext _db)
+    {
+        db = _db;
+    }
+
+    private IDictionary<Type, object> repositories = new Dictionary<Type, object>();
+
+    public IGenericRepository<TEnitity> GetRepository<TEnitity>() where TEnitity : BaseEntity
+    {
+        Type entityType = typeof(TEnitity);
+        if (!repositories.ContainsKey(entityType))
+            repositories[entityType] = new GenericRepository<TEnitity>(db);
+
+        return repositories[entityType] as IGenericRepository<TEnitity> ?? throw new Exception("Repository not found");
+    }
+
+    public async Task Commit()
+    {
+        try
+        {
+            await db.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            //TODO:: Log the exception
+            throw new InvalidOperationException("An error occurred while saving changes.", ex);
+        }
+    }
+
+    public async Task Rollback()
+    {
+        await db.DisposeAsync();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    protected virtual async Task Dispose(bool disposing)
+    {
+        if (!disposed && disposing)
+            await db.DisposeAsync();
+        disposed = true;
+    }
+}
